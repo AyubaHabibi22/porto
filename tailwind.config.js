65555a60f45587c502839eb4a7aac730f61e540c
AyubaHabibi22 <2255habibi2255@gmail.com>
2025-05-10 19:04:19 +0700

    Upload project pertama

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#64748b',
+        
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
 }
